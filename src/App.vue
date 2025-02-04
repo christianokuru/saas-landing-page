2905f98b2e67296c66fb19cae78ff5ab09354d04
christianokuru <okuruchristian@gmail.com>
2025-02-05 00:12:59 +0100

    Boiler plates

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+
+  </header>
+  <router-view />
+</template>
+
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <h1 class="font-bold text-5xl text-blue-500">hellooooo</h1>
+</template>
